5019732c8ae962fb363fe1e6e71a59208c882fb1
Skh121 <sabinkhadka121@gmail.com>
2024-01-26 19:31:53 +0545

    Update Saleberry

---
+++ Saleberry/src/main/java/com/groupc/saleberry/View/Global.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.groupc.saleberry.View;
+
+import com.groupc.saleberry.Model.RegisterModel;
+
+/**
+ *
+ * @author sabin
+ */
+    public class Global{
+    public static RegisterModel user;
+}
+
